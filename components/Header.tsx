4e50a25867c36b97568208fbdd3b30b9f5e0ae85
Andrew Glago <andrewglago1@gmail.com>
2021-12-12 19:26:15 +0000

    update header height

--- components/Header.tsx
+++ components/Header.tsx
@@ -24,7 +24,7 @@ const Header = () => {
         animate && 'shadow-md '
       }`}
     >
-      <header className="max-w-[1440px] m-auto flex justify-between xs:p-6 items-center">
+      <header className="max-w-[1440px] m-auto flex justify-between px-2 xs:px-6 xs:py-3 items-center">
         <div className="flex items-center gap-3">
           <Image src={Logo} alt="Quadro Logo" height={70} width={219.59} />
           {/* <h1 className="uppercase font-bold text-2xl">Quadro</h1> */}
